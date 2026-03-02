610329425650646710d9ae64be4140c68b609999
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2026-03-02 09:55:30 +0700

    tambah UserDto untuk respon data user

---
+++ demo/demo/src/main/java/com/example/demo/model/dto/UserDto.java
@@ -0,0 +1,12 @@
+package com.example.demo.model.dto;
+
+import lombok.Data;
+
+@Data // no usages
+public class UserDto {
+
+    private String id;
+    private String name;
+    private int age;
+
+}
